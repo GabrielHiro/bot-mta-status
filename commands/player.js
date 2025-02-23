659c680c5583dcee658b069965414679bb909cef
Gabriel Hiro Furukawa <hirogamer10@gmail.com>
2025-02-22 22:54:05 -0300

    rename command and update descriptions to Portuguese

--- commands/player.js
+++ commands/player.js
@@ -7,8 +7,8 @@ const {
 } = require("discord.js");
 
 module.exports = {
-  name: "jugadores",
-  description: "Jugadores activos en el servidor.",
+  name: "jogadores",
+  description: "Jogadores ativos no servidor.",
   options: [],
 
   async execute(client, interaction, config, db) {
@@ -19,24 +19,27 @@ module.exports = {
 
     const players = [];
 
+    // Consulta o servidor MTA:SA para obter informações dos jogadores
     const mtasa = await gamedig.query({
       type: "mtasa",
       host: server.ip,
       port: server.port,
     });
 
+    // Adiciona os jogadores à lista com formatação específica
     for (const i in mtasa.players) {
       players.push(
         `:woman_curly_haired::skin-tone-1: **|** \`${mtasa.players[i].name}\` **|** **${mtasa.players[i].raw.ping}ms**`
       );
     }
 
-    var max = 25;
-    var min = 0;
+    let max = 25;
+    let min = 0;
 
     const a = players.slice(min, max);
     const v1 = a.join("\n");
 
+    // Cria o embed com a lista de jogadores
     const embed = new EmbedBuilder()
       .setColor(config.color || 777)
       .setAuthor({ name: `${mtasa.name}` })
@@ -47,18 +50,19 @@ module.exports = {
         iconURL: `${user.displayAvatarURL()} `,
       });
 
+    // Cria a linha de botões para navegação
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
-        .setCustomId("geri")
+        .setCustomId("anterior")
         .setEmoji("⬅️")
         .setStyle(ButtonStyle.Secondary),
       new ButtonBuilder()
-        .setCustomId("sayfa")
+        .setCustomId("pagina")
         .setLabel(`${min}/${max}`)
         .setDisabled(true)
         .setStyle(ButtonStyle.Secondary),
       new ButtonBuilder()
-        .setCustomId("ileri")
+        .setCustomId("proximo")
         .setEmoji("➡️")
         .setStyle(ButtonStyle.Secondary)
     );
@@ -74,14 +78,14 @@ module.exports = {
     });
 
     collector.on("collect", async (i) => {
-      if (i.customId === "ileri") {
+      if (i.customId === "proximo") {
         max += 25;
         min += 25;
 
-        const arti = max;
-        const eski = min;
+        const novoMax = max;
+        const novoMin = min;
 
-        const a = players.slice(eski, arti);
+        const a = players.slice(novoMin, novoMax);
         const v1 = a.join("\n");
 
         const embed = new EmbedBuilder()
@@ -94,20 +98,20 @@ module.exports = {
             iconURL: `${user.displayAvatarURL()} `,
           });
 
-        if (arti >= mtasa.raw.numplayers) {
+        if (novoMax >= mtasa.raw.numplayers) {
           const rowa = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
-              .setCustomId("geri")
+              .setCustomId("anterior")
               .setEmoji("⬅️")
               .setStyle(ButtonStyle.Secondary),
             new ButtonBuilder()
-              .setCustomId("sayfa")
-              .setLabel(`${eski}/${arti}`)
+              .setCustomId("pagina")
+              .setLabel(`${novoMin}/${novoMax}`)
               .setDisabled(true)
               .setStyle(ButtonStyle.Secondary),
             new ButtonBuilder()
               .setDisabled(true)
-              .setCustomId("ileri")
+              .setCustomId("proximo")
               .setEmoji("➡️")
               .setStyle(ButtonStyle.Secondary)
           );
@@ -116,16 +120,16 @@ module.exports = {
         } else {
           const row = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
-              .setCustomId("geri")
+              .setCustomId("anterior")
               .setEmoji("⬅️")
               .setStyle(ButtonStyle.Secondary),
             new ButtonBuilder()
-              .setCustomId("sayfa")
-              .setLabel(`${eski}/${arti}`)
+              .setCustomId("pagina")
+              .setLabel(`${novoMin}/${novoMax}`)
               .setDisabled(true)
               .setStyle(ButtonStyle.Secondary),
             new ButtonBuilder()
-              .setCustomId("ileri")
+              .setCustomId("proximo")
               .setEmoji("➡️")
               .setStyle(ButtonStyle.Secondary)
           );
@@ -134,14 +138,14 @@ module.exports = {
         }
       }
 
-      if (i.customId === "geri") {
+      if (i.customId === "anterior") {
         max -= 25;
         min -= 25;
 
-        const arti = max;
-        const eski = min;
+        const novoMax = max;
+        const novoMin = min;
 
-        const a = players.slice(eski, arti);
+        const a = players.slice(novoMin, novoMax);
         const v1 = a.join("\n");
 
         const embed = new EmbedBuilder()
@@ -154,21 +158,21 @@ module.exports = {
             iconURL: `${user.displayAvatarURL()} `,
           });
 
-        if (arti === 25) {
+        if (novoMax === 25) {
           const rowa = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
-              .setCustomId("geri")
+              .setCustomId("anterior")
               .setDisabled(true)
               .setEmoji("⬅️")
               .setStyle(ButtonStyle.Secondary),
             new ButtonBuilder()
-              .setCustomId("sayfa")
-              .setLabel(`${eski}/${arti}`)
+              .setCustomId("pagina")
+              .setLabel(`${novoMin}/${novoMax}`)
               .setDisabled(true)
               .setStyle(ButtonStyle.Secondary),
             new ButtonBuilder()
               .setDisabled(false)
-              .setCustomId("ileri")
+              .setCustomId("proximo")
               .setEmoji("➡️")
               .setStyle(ButtonStyle.Secondary)
           );
@@ -177,16 +181,16 @@ module.exports = {
         } else {
           const row = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
-              .setCustomId("geri")
+              .setCustomId("anterior")
               .setEmoji("⬅️")
               .setStyle(ButtonStyle.Secondary),
             new ButtonBuilder()
-              .setCustomId("sayfa")
-              .setLabel(`${eski}/${arti}`)
+              .setCustomId("pagina")
+              .setLabel(`${novoMin}/${novoMax}`)
               .setDisabled(true)
               .setStyle(ButtonStyle.Secondary),
             new ButtonBuilder()
-              .setCustomId("ileri")
+              .setCustomId("proximo")
               .setEmoji("➡️")
               .setStyle(ButtonStyle.Secondary)
           );
